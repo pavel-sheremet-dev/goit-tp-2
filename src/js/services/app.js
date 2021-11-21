b19488078b1dcea64b4ae7603695696c22275e6a
YarinaLoboda <yarina.loboda@gmail.com>
2021-11-21 23:49:17 +0200

    infinitescroll -> topRatedShow()

--- src/js/services/app.js
+++ src/js/services/app.js
@@ -1,6 +1,10 @@
 import { showAlert, showError } from '../vendors/alerts';
 import ALERTS from '../data/alertsMsgs';
 
+
+
+
+
 export default class Application {
   #API_KEY = '6759d249684e99a49309af19f6af0ff2';
   #BASE_API_URL = 'https://api.themoviedb.org/3';
@@ -21,6 +25,7 @@ export default class Application {
 
   init = () => {
     // Сюда добавляем все действия, которые должны произойти при загрузке стартовой страницы, например слушатели событий, отрисовка популярных фильмов.
+    this.topRatedShow();
     this.loadListeners();
   };
 
@@ -53,6 +58,64 @@ export default class Application {
   //     });
   //   }
   // };
+
+  topRatedShow = () => {
+  
+const container = document.querySelector('.js-cards__list');
+
+const infScroll = new InfiniteScroll(container, {
+  
+  path: function () {
+         return `https://api.themoviedb.org/3/movie/top_rated?api_key=6759d249684e99a49309af19f6af0ff2&language=en-US&page=${this.pageIndex}`;
+  },
+  // load response as JSON
+  responseBody: 'json',
+  status: '.page-load-status',
+  history: false,
+});
+
+    // use element to turn HTML string into elements
+let proxyElem = document.createElement('li');
+
+infScroll.on('load', function (body) {
+ 
+  // compile body data into HTML
+  var itemsHTML = body.results.map(getTopRatedMarkUp).join('');
+
+  // convert HTML string into elements
+  proxyElem.innerHTML = itemsHTML;
+ 
+  // append item elements
+  
+  container.append( ...proxyElem.children );
+});
+
+// load initial page
+infScroll.loadNextPage();
+
+//------------------//
+
+ function getTopRatedMarkUp({ id, poster_path, title, vote_average, genre_ids, release_date}) {
+
+    return `
+   <li class="cards__item">
+  <a href="#" class="cards__link link" data-id="${id}">
+          <div class="cards__loader"></div>
+          <img
+            class="cards__img"
+            src ="https://image.tmdb.org/t/p/original/${poster_path}"
+            alt="постер фильма ${title}"
+          />
+          <h3 class="cards__title">${title}</h3>
+          <p class="cards__genres">[${[...genre_ids]} | ${release_date.substring(0, 4)}]</p>
+          <p class="cards__rating">${vote_average}</p>
+        </a>
+    </li>`;
+
+}
+   
+}
+
 
   // Пример стандартной фукнции (метода)
 
